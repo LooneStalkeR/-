6cef78a7ded49207edb6b0579b534dd6a88f83eb
Ivan Romanov <105872764+LooneStalkeR@users.noreply.github.com>
2022-05-29 17:42:11 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -7,12 +7,12 @@ struct row
 {
     int x;
     int h;
-} rows[200];
+} rows[240];
 
 
 int main()
 {
-    RenderWindow window(VideoMode(240, 180), "sort");
+    RenderWindow window(VideoMode(240, 200), "sort");
     RectangleShape rectangle;
     Event event;
 
@@ -28,29 +28,63 @@ int main()
         }
         if (isCreated) 
         {
-            for(int i = 1;i < rows[index].h; i++){
-                for (int j = i; j > 0 && rows[index].h - 1 > rows[index].h; j--)
-                {
+            //int z;
+            //int j;
+
+            //while (rows[index].h > rows[index - 1].h)
+             //if(rows[index].h>rows[index+1].h)//for (int i = 1; i < rows[index].h; i++)
+            
+                 
+                //for (int i = 1; i < rows[index].h; i++) {
+                    //z = i;
+                    //j = i - 1;
+                    //while (i > rows[index - 1].h > z)
+
+                    //std::swap(rows[index].h, rows[index + 1].h);
+                    //z = rows[index].h;
+                    while (rows[index].h > rows[index + 1].h) {
+                        std::swap(rows[index].h, rows[index + 1].h);
+                    }
+                    //rows[j + 1].h = rows[index].h;
+                    //j = j - 1;
+                
+            
+
+            /*int i;
+            while (i=1 < rows[index].h)
+            {
+                if (i == 0)
+                    i++;
+                if (rows[index].h >= rows[index - 1].h)
+                    i++;
+                else {
                     std::swap(rows[index].h, rows[index - 1].h);
+                    i--;
                 }
+            }*/
+
+
+            //rows[j + 1].h = z;
+            //std::swap(rows[index].h, rows[index+1].h);
+
         }
 
         }
         if (!isCreated)
         {
-            for (int i = 0; i < 200; ++i)
+            for (int i = 0; i < 240; ++i)
             {
                 rows[i].x = i;
-                rows[i].h = 1 + rand() % 180;
+                rows[i].h = 1 + rand() % 100;
             }
             isCreated = true;
         }
 
         window.clear(Color::Black);
-        for (int i = 0; i < 200; ++i)
+        for (int i = 0; i < 240; ++i)
         {
             rectangle.setSize(Vector2f(1, rows[i].h));
-            rectangle.setPosition(Vector2f(i, 180));
+            rectangle.setPosition(Vector2f(i++, 200));
             rectangle.setRotation(180);
             rectangle.setFillColor(Color::Blue);
             if (i == index) rectangle.setFillColor(Color::Red);
@@ -59,7 +93,7 @@ int main()
         window.display();
 
         index++;
-        if (index >= 199)
+        if (index >= 240)
             index = 0;
     }
 
